d5f24b29045370291640db32169abdc75ea3a7eb
ujjwol <ujjwolneupane75@gmail.com>
2022-08-20 18:54:00 +0545

    hello

---
+++ out/_next/static/chunks/pages/index-55cfc6b93bd92eab.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5557:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(3174)}])}},function(n){n.O(0,[445,13,617,158,609,583,118,774,888,179],(function(){return _=5557,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
---
+++ out/_next/static/chunks/pages/scrappedData/ScrappedData-7db5db6116a0bc12.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[859],{7585:function(n,_,a){(window.__NEXT_P=window.__NEXT_P||[]).push(["/scrappedData/ScrappedData",function(){return a(2392)}])}},function(n){n.O(0,[445,13,617,158,609,583,118,774,888,179],(function(){return _=7585,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
